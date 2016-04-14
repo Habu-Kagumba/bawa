34e5377570ebadb8f705f724648eee56d443702c
Herbert Kagumba <herbert.kagumba@andela.com>
2016-04-14 14:50:38 +0300

    fix (CI): Fix travis build error.
    - place landing spec within `with` block.

--- spec/features/landing_pages_spec.rb
+++ spec/features/landing_pages_spec.rb
@@ -8,8 +8,10 @@ RSpec.feature "LandingPages", type: :feature do
   scenario "the landing page should have the applications tagline." do
     visit root_path
 
-    expect(page).to have_selector(
-      "h1", text: "Bawa Secure and reliable flight booking."
-    )
+    within "div.content" do
+      expect(page).to have_selector(
+        "h1", text: "Bawa Secure and reliable flight booking."
+      )
+    end
   end
 end

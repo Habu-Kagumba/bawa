7620bd9c97ddae035fbef33ea5a9f8cebb12a5d5
Herbert Kagumba <habukagumba@gmail.com>
2016-04-14 13:02:40 +0300

    test (coverage) Add coveralls configuration.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -7,9 +7,11 @@ require 'spec_helper'
 require 'rspec/rails'
 require 'capybara/rspec'
 require 'database_cleaner'
+require 'coveralls'
 
 # Add additional requires below this line. Rails is not loaded until this point!
 
+Coveralls.wear!
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
 # run as spec files by default. This means that files in spec/support that end

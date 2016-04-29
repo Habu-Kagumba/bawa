5a5e4256d0a77812fa292f0034ba014f5beb18eb
Herbert Kagumba <habukagumba@gmail.com>
2016-04-29 12:10:19 +0300

    feat(booking and flights): add booking and flight
    - add booking and flight models
    - override faker to provide extra methods (add flight & customise price)
    
    finishes #117938577

---
+++ spec/models/passenger_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe Passenger, type: :model do
+  subject(:passenger) do
+    create(:flight)
+    create(:booking)
+    create(:passenger)
+  end
+
+  describe "Validation" do
+    context "when I create a booking" do
+      it "should be valid" do
+        expect(passenger).to be_valid
+      end
+    end
+  end
+end

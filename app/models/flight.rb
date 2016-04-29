46918fe427133a1259ba68dd927325f9676e1784
Herbert Kagumba <habukagumba@gmail.com>
2016-04-29 19:08:27 +0300

    fix(style-guide): fix rubocop styles

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,35 +1,31 @@
 class Flight < ActiveRecord::Base
-  belongs_to :departure_location, class_name: 'Airport'
-  belongs_to :arrival_location, class_name: 'Airport'
+  belongs_to :departure_location, class_name: "Airport"
+  belongs_to :arrival_location, class_name: "Airport"
 
-  scope :location, -> (departure_location_id) {
-    where departure_location_id: departure_location_id
-  }
+  scope :location, -> (loc_id) { where departure_location_id: loc_id }
 
-  scope :destination, -> (arrival_location_id) {
-    where arrival_location_id: arrival_location_id
-  }
+  scope :destination, -> (loc_id) { where arrival_location_id: loc_id }
 
-  scope :when, -> (departure_date) {
-    where("departure_date > ?", Date.parse(departure_date).to_time)
-      .where("departure_date < ?", (Date.parse(departure_date) + 1).to_time)
-  }
+  scope :when, lambda do |ddate|
+    where("departure_date > ?", Date.parse(ddate).to_time).
+      where("departure_date < ?", (Date.parse(ddate) + 1).to_time)
+  end
 
   validates :departure_location_id, :arrival_location_id, :flight_number,
             :airline, :departure_date, :arrival_date,
             presence: true
 
   def departure_time
-    self.departure_date.strftime("%H:%M")
+    departure_date.strftime("%H:%M")
   end
 
   def arrival_time
-    self.arrival_date.strftime("%H:%M")
+    arrival_date.strftime("%H:%M")
   end
 
   class << self
     def filter(filtering_params)
-      results = self.where(nil)
+      results = where(nil)
       filtering_params.each do |key, value|
         results = results.public_send(key, value) if value.present?
       end

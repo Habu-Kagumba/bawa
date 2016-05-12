3d741b0086835ef435f7cb1a368f6a263c592cd3
Herbert Kagumba <herbert.kagumba@andela.com>
2016-05-12 20:48:30 +0300

    bg(sendgrid): change to sendgrid.
    Chang to sendgrid for emails.
    
    [#119447573]

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,13 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+ActionMailer::Base.smtp_settings = {
+  :user_name => ENV['gmail_username'],
+  :password => ENV['gmail_password'],
+  :domain => 'heroku.com',
+  :address => 'smtp.sendgrid.net',
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}

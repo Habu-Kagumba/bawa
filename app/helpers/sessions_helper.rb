3a894032c2c5132938a7ff9ab63acaea608e7f10
Herbert Kagumba <habukagumba@gmail.com>
2016-05-31 10:20:28 +0300

    chore(user): refactor user logic
    - Introduce the UserAuthenticator service object.
    - Refactor the SessionsHelper, UsersController and ApplicationController
    
    [#119826095]

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,27 +1,11 @@
 module SessionsHelper
-  def log_in(user)
-    session[:user_id] = user.id
-  end
-
-  def remember(user)
-    user.remember
-    cookies.permanent.signed[:user_id] = user.id
-    cookies.permanent[:remember_token] = user.remember_token
-  end
-
-  def log_out
-    forget(current_user)
-    session.delete(:user_id)
-    @current_user = nil
-  end
-
   def current_user
     if user_id = session[:user_id]
       @current_user ||= User.find_by(id: user_id)
     elsif user_id = cookies.signed[:user_id]
       user = User.find_by(id: user_id)
       if user && user.authenticated?(cookies[:remember_token])
-        log_in user
+        session[:user_id] = user.id
         @current_user = user
       end
     end
@@ -33,28 +17,5 @@ module SessionsHelper
 
   def logged_in?
     !current_user.nil?
-  end
-
-  def forget(user)
-    user.forget
-    cookies.delete(:user_id)
-    cookies.delete(:remember_token)
-  end
-
-  def redirect_on_login(where)
-    redirect_to(session[:url] || where)
-    session.delete(:url)
-  end
-
-  def store_url
-    session[:url] = request.fullpath
-  end
-
-  def logged_in_user
-    unless logged_in?
-      store_url
-      flash[:error] = "Please log in to continue"
-      redirect_to login_url
-    end
   end
 end

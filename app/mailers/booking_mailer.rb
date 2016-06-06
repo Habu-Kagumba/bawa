876aa390127dfd145571b57a1154b8679d792465
Herbert Kagumba <habukagumba@gmail.com>
2016-06-06 20:33:18 +0300

    chore(mailer): refactor instance variables
    Switch from instance variables to locals
    
    [#120905585]

--- app/mailers/booking_mailer.rb
+++ app/mailers/booking_mailer.rb
@@ -2,10 +2,15 @@ class BookingMailer < ApplicationMailer
   default from: "no-reply@bawa.com"
 
   def successful_booking(booking, user)
-    @booking = booking
-    @user = user
-    @flights = FlightPresenter.new(@booking.flight)
-    @url = URI.join("https://bawa.herokuapp.com/bookings/", @booking.slug)
-    mail(to: @user.email, subject: "Booking successful.")
+    locals = {
+      booking: booking,
+      user: user,
+      flights: FlightPresenter.new(booking.flight),
+      url: URI.join("https://bawa.herokuapp.com/bookings/", booking.slug)
+    }
+    mail(to: user.email, subject: "Booking successful.") do |format|
+      format.text { render locals: locals }
+      format.html { render locals: locals }
+    end
   end
 end

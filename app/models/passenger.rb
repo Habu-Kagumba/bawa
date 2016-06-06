ee08e24d590938633d719038b593252775c040d3
Herbert Kagumba <herbert.kagumba@andela.com>
2016-06-06 20:39:59 +0300

    chore(refactor): refactor long statements
    Adhere to ruby style guide and refactor multiline statements
    
    [#120905585]

--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -1,7 +1,10 @@
 class Passenger < ActiveRecord::Base
   belongs_to :booking
 
-  validates :first_name, :last_name, :email,
+  validates :first_name,
+            :last_name,
+            :email,
             presence: true
+
   validates :email, email: true
 end
